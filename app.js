ffc5d2a4c92aeda17f842b59c6959d2bb20c7c4a
David Millan <hello@davidmillan.me>
2019-09-15 11:58:02 +0100

    Completed S4.51. Implementing Our 'Hold' Function and the DRY Principle

--- app.js
+++ app.js
@@ -18,6 +18,26 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
+// Function to change player
+function nextPlayer() {
+    // Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; //ternary operator
+
+    // Score back to 0
+    roundScore = 0;
+
+    // Reset round score boards
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    // Change active player class in the DOM
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    // Hide dice
+    document.querySelector('.dice').style.display = 'none';
+};
+
 
 // Generate a random number between 1 and 6
 // dice = Math.floor(Math.random() * 6) + 1;
@@ -62,25 +82,28 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
-
-        // Next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; //ternary operator
-
-        // Score back to 0
-        roundScore = 0;
-
-        // Reset round score boards
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        // Change active player class in the DOM
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-        // Hide dice
-        document.querySelector('.dice').style.display = 'none';
+        nextPlayer();
     }
 
 });
 
+// Apply functionality to hold button
+// Event listener that triggers an anonymous function
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    // Add round score to global player score
+    scores[activePlayer] += roundScore;
 
+    // Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    // Check if player won the game
+    if (scores[activePlayer] >= 20) {
+        document.getElementById('name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        // Change player
+        nextPlayer();
+    };
+});
